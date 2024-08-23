13c5e2d9ed1a239875b7f3232b4bfe2bacfde4ca
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2024-08-23 12:56:29 +0700

    fix: migrate from script setup

--- src/datatable/PrimevueDatatable.vue
+++ src/datatable/PrimevueDatatable.vue
@@ -16,63 +16,6 @@
     </div>
 </template>
 
-<script setup>
-import './style.css';
-import { watch, ref } from 'vue';
-const props = defineProps({
-    paginator: {
-        type: Boolean,
-        default: false,
-    },
-    showGridlines: {
-        type: Boolean,
-        default: false,
-    },
-    stripedRows: {
-        type: Boolean,
-        default: false,
-    },
-    size: {
-        type: String,
-        default: 'normal',
-    },
-    rows: {
-        type: Number,
-        default: 5,
-    },
-    headers: {
-        type: String,
-        default: '',
-    },
-    data: {
-        type: Array,
-        default: () => [],
-    },
-});
-const columns = ref([]);
-
-watch(
-    () => [props.headers, props.data],
-    () => {
-        const cols = [];
-        const headers = props.headers.split(',');
-        let idx = 0;
-        if (!props.data.length) return cols;
-        for (const property in props.data[0]) {
-            const header = headers[idx] || property;
-            cols.push({ field: property, header: capitalizeFirstLetter(header) });
-            idx += 1;
-        }
-        columns.value = cols;
-    },
-    { immediate: true }
-);
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-</script>
-
 <script>
 import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config';
@@ -80,12 +23,78 @@ import PVDataTable from 'primevue/datatable';
 import PVColumn from 'primevue/column';
 
 export default {
+    props: {
+        paginator: {
+            type: Boolean,
+            default: false,
+        },
+        showGridlines: {
+            type: Boolean,
+            default: false,
+        },
+        stripedRows: {
+            type: Boolean,
+            default: false,
+        },
+        size: {
+            type: String,
+            default: 'normal',
+        },
+        rows: {
+            type: Number,
+            default: 5,
+        },
+        headers: {
+            type: String,
+            default: '',
+        },
+        data: {
+            type: Array,
+            default: () => [],
+        },
+    },
     components: {
         PVDataTable,
         PVColumn,
     },
+    data() {
+        return {
+            columns: [],
+        };
+    },
     beforeCreate: function () {
         this.$.appContext.app.use(PrimeVue, { theme: { preset: Aura } });
+    },
+    computed: {
+        headersAndValue() {
+            return [this.headers, this.data];
+        },
+    },
+    watch: {
+        headersAndValue: {
+            handler() {
+                this.updateColumns();
+            },
+            deep: true,
+            immediate: true,
+        },
+    },
+    methods: {
+        capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        },
+        updateColumns() {
+            const cols = [];
+            const headers = this.headers.split(',');
+            let idx = 0;
+            if (!this.data.length) return cols;
+            for (const property in this.data[0]) {
+                const header = headers[idx] || property;
+                cols.push({ field: property, header: this.capitalizeFirstLetter(header) });
+                idx += 1;
+            }
+            this.columns = cols;
+        },
     },
 };
 </script>
